15cfcdc53697f050370e6498e88602220fb66dfe
Gopi Nelluri <avgh3@umkc.edu>
2022-03-02 19:26:06 -0600

    Saga Reviews

--- Project/src/app/app.module.ts
+++ Project/src/app/app.module.ts
@@ -6,13 +6,27 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MyWishlistComponent } from './my-wishlist/my-wishlist.component';
 import { CategoriesComponent } from './categories/categories.component';
+import { MyPolesComponent } from './my-poles/my-poles.component';
+import { SigninComponent } from './signin/signin.component';
+import { NowplayingComponent } from './nowplaying/nowplaying.component';
+import { SignupComponent } from './signup/signup.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { MyreviewsComponent } from './myreviews/myreviews.component';
+import { CatalogComponent } from './catalog/catalog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     MyWishlistComponent,
-    CategoriesComponent
+    CategoriesComponent,
+    MyPolesComponent,
+    SigninComponent,
+    NowplayingComponent,
+    SignupComponent,
+    AboutUsComponent,
+    MyreviewsComponent,
+    CatalogComponent
   ],
   imports: [
     BrowserModule,
